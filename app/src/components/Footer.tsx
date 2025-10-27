9a57689c01a15f744e8b95f799c1d19424cae831
Raphael Laude <raphlaude@gmail.com>
2025-10-27 17:40:06 -0600

    update footer width

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-100 border-t border-gray-300 mt-auto">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         {/* Icon Links Section */}
         <div className="flex justify-start mb-6">
           <a
