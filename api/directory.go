86ff9b81fa47e08231bcf45be7da7048c06c137d
Raphael Laude <raphlaude@gmail.com>
2025-12-28 02:29:56 -0800

    support multiple tokens

--- api/directory.go
+++ api/directory.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"context"
-	_ "embed"
 	"errors"
+	"fmt"
+	"regexp"
+	"strings"
 
 	"connectrpc.com/connect"
 	"github.com/jmoiron/sqlx"
 	directoryv1 "github.com/raphaellaude/usaschooldata/api/directory/v1"
 )
 
-//go:embed sql/search_directory.sql
-var search_directory string
+var nonAlphanumeric = regexp.MustCompile(`[^a-zA-Z0-9]+`)
 
 type DirectoryHandler struct {
 	db *sqlx.DB
@@ -22,8 +23,45 @@ func NewDirectoryHandler(db *sqlx.DB) *DirectoryHandler {
 }
 
 func (h *DirectoryHandler) GetMatchingSchools(ctx context.Context, req *directoryv1.GetMatchingSchoolsRequest) (*directoryv1.GetMatchingSchoolsResponse, error) {
+	// Replace non-alphanumeric characters with spaces, then split into tokens
+	normalized := nonAlphanumeric.ReplaceAllString(req.SearchTerm, " ")
+	tokens := strings.Fields(normalized)
+	if len(tokens) == 0 {
+		return nil, connect.NewError(
+			connect.CodeInvalidArgument,
+			errors.New("search term cannot be empty"),
+		)
+	}
+
+	// Build dynamic WHERE conditions for each token
+	var conditions []string
+	args := make(map[string]interface{})
+	for i, token := range tokens {
+		paramName := fmt.Sprintf("token%d", i)
+		conditions = append(conditions, fmt.Sprintf("hasTokenCaseInsensitive(sch_name, :%s)", paramName))
+		args[paramName] = token
+	}
+
+	// Build the final query with all conditions
+	whereClause := strings.Join(conditions, " AND ")
+	query := fmt.Sprintf(`SELECT
+    ncessch,
+    sch_name,
+    school_year
+FROM "directory"
+WHERE school_year_no = 1 AND
+    %s
+ORDER BY school_year DESC
+LIMIT 10 OFFSET 0`, whereClause)
+
+	// Execute query with named parameters
 	var results []*directoryv1.SchoolSearch
-	err := h.db.SelectContext(ctx, &results, search_directory, req.SearchTerm)
+	namedQuery, namedArgs, err := sqlx.Named(query, args)
+	if err != nil {
+		return nil, err
+	}
+	namedQuery = h.db.Rebind(namedQuery)
+	err = h.db.SelectContext(ctx, &results, namedQuery, namedArgs...)
 	if err != nil {
 		return nil, err
 	}
--- api/sql/search_directory.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    ncessch,
-    sch_name,
-    school_year
-FROM "directory"
-WHERE school_year_no = 1 AND
-    hasTokenCaseInsensitive(sch_name, ?)
-ORDER BY school_year DESC
-LIMIT 10 OFFSET 0;
