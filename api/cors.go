3c6bc8680f45a02ec8b2517e3f1954a76081a43b
Raphael Laude <raphlaude@gmail.com>
2025-12-28 02:07:11 -0800

    clean up unused code

--- api/cors.go
+++ api/cors.go
@@ -1,41 +1,9 @@
 package main
 
 import (
-	"context"
 	"net/http"
 	"strings"
-
-	"connectrpc.com/connect"
 )
-
-// newCORSInterceptor creates a Connect interceptor that handles CORS
-func newCORSInterceptor(allowedOrigins []string) connect.UnaryInterceptorFunc {
-	return func(next connect.UnaryFunc) connect.UnaryFunc {
-		return func(ctx context.Context, req connect.AnyRequest) (connect.AnyResponse, error) {
-			// Get origin from request header
-			if header, ok := req.Header()["Origin"]; ok && len(header) > 0 {
-				origin := header[0]
-
-				// Check if origin is allowed
-				if isOriginAllowed(origin, allowedOrigins) {
-					// Set CORS headers in response
-					if res, err := next(ctx, req); err != nil {
-						return res, err
-					} else {
-						res.Header().Set("Access-Control-Allow-Origin", origin)
-						res.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-						res.Header().Set("Access-Control-Allow-Headers", "Content-Type, Connect-Protocol-Version, Connect-Timeout-Ms")
-						res.Header().Set("Access-Control-Expose-Headers", "Connect-Protocol-Version, Connect-Timeout-Ms")
-						res.Header().Set("Access-Control-Max-Age", "7200")
-						return res, nil
-					}
-				}
-			}
-
-			return next(ctx, req)
-		}
-	}
-}
 
 // isOriginAllowed checks if an origin is in the allowed list
 func isOriginAllowed(origin string, allowedOrigins []string) bool {
